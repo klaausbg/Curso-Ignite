7d51b24b3e404a7e3491183e5722c74d6e89ea71
Klaus Boeger <klausboeger@hotmail.com>
2024-09-18 01:40:11 -0300

    Typescript

--- src/index.tsx
+++ src/index.tsx
@@ -1,2 +1,6 @@
-import { render } from "react-dom";
+import React from "react";
+import ReactDOM from "react-dom/client"; // Use the React 18 API for rendering
 import { App } from "./App";
+
+const root = ReactDOM.createRoot(document.getElementById("root")!); // Non-null assertion to ensure TypeScript knows 'root' will exist
+root.render(<App />);
